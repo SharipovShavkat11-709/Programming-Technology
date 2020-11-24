6da66042a0051732e6d4b063f421e3f849deabbe
SharipovShavkat11-709 <31937258+SharipovShavkat11-709@users.noreply.github.com>
2020-11-24 23:04:16 +0800

    Add files via upload

---
+++ Gadgets/Gadgets/SmartPhone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gadgets
+{
+    //Класс под смартфоны
+    public class SmartPhone : Gadget
+    {
+        public int SimCardSlotsCount = 0; //количество слотов под sim карту
+        public int CamerasMp = 0; //количество мегапикселей у камеры
+        public int BatteryCapacity = 0; // емкость батареи
+
+        //Получение информации о гаджетах
+        public override String GetInfo()
+        {
+            var str = GadgetName;//название гаджета
+            str += base.GetInfo(); // а тут заменил вызовом GetInfo базового класса, т.е. Gadgets
+            str += String.Format("\nКоличество Sim-карт: {0}", this.SimCardSlotsCount);
+            str += String.Format("\nКамера, Мп: {0}", this.CamerasMp);
+            str += String.Format("\nБатарея: {0}", this.BatteryCapacity);
+            return str;
+        }
+
+        public static SmartPhone Generate()
+        {
+            return new SmartPhone
+            {
+                GadgetName = "Смартфон",
+                DisplaySize = GetRandomDoubleNumber(4, 7), // размер дисплея от 4.0 до 7.0
+                SimCardSlotsCount = random.Next(1, 3),//количество сим карт от 1 до 2
+                CamerasMp = random.Next(16, 108),//количесвто мегапикселей от 16 до 108       
+                BatteryCapacity = random.Next(2000, 6000)//емкость батареи от 2200 до 6000
+            };
+        }
+    }
+}
---
+++ Gadgets/Gadgets/Tablet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gadgets
+{
+    //Класс под планшеты, тоже наследует от класса Gadget
+    public class Tablet : Gadget
+    {
+        public bool HasCameras = false; //наличие камеры
+        public int ScreenDPI = 0; // dpi экрана 
+
+        public override String GetInfo()
+        {
+            var str = GadgetName;//название гаджета
+            str += base.GetInfo(); // а тут заменил вызовом GetInfo базового класса, т.е. Gadgets
+            str += String.Format("\nНаличие камеры: {0}", this.HasCameras);
+            str += String.Format("\ndpi: {0}", this.ScreenDPI);
+            return str;
+        }
+
+        public static Tablet Generate()
+        {
+            return new Tablet
+            {
+                GadgetName = "Планшет",
+                DisplaySize = GetRandomDoubleNumber(7, 12), //размер дисплея от 7 до 12
+                HasCameras = random.Next(2) == 0, // наличие камеры
+                ScreenDPI = random.Next(200, 520) // dpi экрана от 200 до 520
+            };
+        }
+    }
+}
